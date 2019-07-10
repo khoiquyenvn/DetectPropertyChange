acca9cf5eef3ae1eccd7203974d25780a34f5a6f
khoiquyenvn <khoiquyenvn@gmail.com>
2019-07-10 15:21:58 +0700

    add src

---
+++ ConsoleApp1/Extension.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    static class Extentions
+    {
+        public static List<Variance> DetailedCompare<T>(T val1, T val2)
+        {
+            List<Variance> variances = new List<Variance>();
+            CompareField(val1, val2, variances);
+            ComparePros(val1, val2, variances);
+
+            return variances;
+        }
+
+        private static void CompareField<T>(T val1, T val2, List<Variance> variances)
+        {
+            FieldInfo[] fi = val1.GetType().GetFields();
+            foreach (FieldInfo f in fi)
+            {
+                Variance v = new Variance();
+                v.Prop = f.Name;
+                v.valA = f.GetValue(val1);
+                v.valB = f.GetValue(val2);
+                if (!v.valA.Equals(v.valB))
+                    variances.Add(v);
+            }
+        }
+
+        private static void ComparePros<T>(T val1, T val2, List<Variance> variances)
+        {
+            PropertyInfo[] pi = val1.GetType().GetProperties();
+            foreach (PropertyInfo p in pi)
+            {
+                Variance v = new Variance();
+                v.Prop = p.Name;
+                
+
+                if (p.PropertyType.Namespace == "System.Collections.Generic")
+                {
+                    dynamic lst1 = p.GetValue(val1);
+                    dynamic lst2 = p.GetValue(val2);
+
+                    if (lst1 != null && lst2 != null)
+                    {
+                        var notExistInList2 = ((IEnumerable<dynamic>)lst1).Except((IEnumerable<dynamic>)lst2).ToList();
+                        var notExistInList1 = ((IEnumerable<dynamic>)lst2).Except((IEnumerable<dynamic>)lst1).ToList();
+
+                        if (notExistInList1.Any() || notExistInList2.Any())
+                        {
+                            v.valA = lst1;
+                            v.valB = lst2;
+                            v.DeleteItems = notExistInList2;
+                            v.ModifyItems = notExistInList1;
+                            variances.Add(v);
+                        }
+                    }
+                }
+                else
+                {
+                    v.valA = p.GetValue(val1);
+                    v.valB = p.GetValue(val2);
+                    if (!v.valA.Equals(v.valB))
+                        variances.Add(v);
+                }
+            }
+        }
+    }
+    class Variance
+    {
+        public string Prop { get; set; }
+        public dynamic valA { get; set; }
+        public dynamic valB { get; set; }
+        public dynamic DeleteItems { get; set; }
+        public dynamic ModifyItems { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SomeCustomClass a = new SomeCustomClass()
+            {
+                AnotherChildClasses = new List<AnotherChildClass> {
+                     new AnotherChildClass { n = 1, m = 2 },
+                     new AnotherChildClass { n = 11, m = 22 }
+                 }
+            };
+            SomeCustomClass b = new SomeCustomClass()
+            {
+                AnotherChildClasses = new List<AnotherChildClass> {
+                    new AnotherChildClass { n = 21, m = 22 },
+                    new AnotherChildClass { n = 1, m = 2 }
+                }
+            };
+            a.x = 100;
+            b.Child.u = 51;
+
+            List<Variance> compareResult = Extentions.DetailedCompare(a,b);
+
+
+            Console.ReadLine();
+        }
+
+        private static void Test_Compare2List()
+        {
+            var lst1 = new List<AnotherChildClass> { new AnotherChildClass { n = 1, m = 2 }, new AnotherChildClass { n = 11, m = 22 } };
+            var lst2 = new List<AnotherChildClass> { new AnotherChildClass { n = 21, m = 22 }, new AnotherChildClass { n = 1, m = 2 } };
+
+            var diff1 = lst1.Except(lst2).ToList();
+            var diff2 = lst2.Except(lst1).ToList();
+        }
+    }
+
+    class SomeCustomClass
+    {
+        public int x = 12;
+        public int y = 13;
+
+        public ChildClass Child { get; set; } = new ChildClass();
+
+        public List<AnotherChildClass> AnotherChildClasses { get; set; } = new List<AnotherChildClass>();
+    }
+
+    partial class ChildClass
+    {
+        public int u = 22;
+        public int v = 23;
+    }
+
+    // try to implement IEquatable in another place (partial)
+    partial class ChildClass : IEquatable<ChildClass>
+    {
+        public bool Equals(ChildClass other)
+        {
+            return this.u == other.u && this.v == other.v;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as ChildClass);
+        }
+
+        public override int GetHashCode()
+        {
+            return u.GetHashCode() ^ v.GetHashCode();
+        }
+    }
+
+    partial class AnotherChildClass : IEquatable<AnotherChildClass>
+    {
+        public int n = 32;
+        public int m = 33;
+        public bool Equals(AnotherChildClass other)
+        {
+            return this.n == other.n && this.m == other.m;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as AnotherChildClass);
+        }
+
+        public override int GetHashCode()
+        {
+            return n.GetHashCode() ^ m.GetHashCode();
+        }
+    }
+}
